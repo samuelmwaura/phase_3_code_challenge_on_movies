7f35b5588da82bffdad681234ba782ac46d176ad
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-09-03 01:34:26 +0300

    Added custom methods in role and Movie models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,11 +6,17 @@ class Movie < ActiveRecord::Base
         Role.create(salary:salary,character_name:character_name,actor:actor,movie:self)
     end
  
-    def all_credits
-       
+    def all_credits #Returns ana array of formatted movie sntrings
+        self.roles.map{|role| "#{role.character_name}: Played by #{role.actor.name}"}
     end
+
+    def fire_actor(actor)  #Takes an actor and removes their role from the movie- This has to happen through the join table where an actor is assigned a role
+        matched_role = Role.find_by(movie_id:self.id,actor_id:actor.id) #We find the exact role in the roles table that has this movie instance for movie_id and the passed in actor as the actor_id
+        matched_role.destroy #Then we delete the found role
+    end #This method works by deleting a row in the roles table because a role has one actor and is in one movie. So when a role is deleted, the actor engagement in the movie is deleted
 
 end
 
 #TEST CASES
 #Movie.first.cast_role(Actor.third,"Brenda Palsey",46006)
+#Movie.first.fire_actor(Actor.first)
--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,7 @@ class Role < ActiveRecord::Base
    belongs_to(:movie) #This gives acess to the #Movie instance method that helps query the owner movie
    belongs_to(:actor)#This gives us #actor
 
+   def credit  #This is a custom method to achieve desired behaviour
+      "#{self.character_name}: Played by #{self.actor.name}"
+   end
 end
