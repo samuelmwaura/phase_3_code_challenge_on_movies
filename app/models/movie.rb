dd003a062b86f48502650f4931eb50b1f094488b
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-09-03 00:46:29 +0300

    Creating association methods for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,14 @@ class Movie < ActiveRecord::Base
     has_many(:roles) #Gives us access #roles
     has_many(:actors,through: :roles) #actors
 
+    def cast_role(actor, character_name, salary) #This is an istance custom method
+        Role.create(salary:salary,character_name:character_name,actor:actor,movie:self)
+    end
+ 
+    def all_credits
+       
+    end
+
 end
 
 #TEST CASES
